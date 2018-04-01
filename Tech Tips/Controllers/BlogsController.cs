d5c0845d93e7135135712c45cd82bdeb1430b147
Muhammad Mohaiminul Islam <IamMohaiminul@Gmail.com>
2018-04-02 01:11:34 +0800

    MVC in Action

---
+++ Tech Tips/Controllers/BlogsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tech_Tips.Models;
+
+namespace Tech_Tips.Controllers
+{
+    public class BlogsController : Controller
+    {
+        // GET: Blogs
+        public ActionResult Random()
+        {
+            var blog = new Blog() {Title = "1st Blog!"};
+
+            return View(blog);
+        }
+    }
+}
