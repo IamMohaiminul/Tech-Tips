b5d2c311b6b427c5cfef5608625522e6d1a98982
Muhammad Mohaiminul Islam <IamMohaiminul@Gmail.com>
2018-04-02 01:15:26 +0800

    Adding a Theme​

--- Tech Tips/App_Start/BundleConfig.cs
+++ Tech Tips/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace Tech_Tips
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/lumen-bootstrap.css",
                       "~/Content/site.css"));
         }
     }
