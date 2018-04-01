cad67ba7fcbe970a04da48c26d0cb5bc911c7763
Muhammad Mohaiminul Islam <IamMohaiminul@Gmail.com>
2018-04-02 00:34:30 +0800

    First Commit

---
+++ Tech Tips/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Tech_Tips.MvcApplication" Language="C#" %>
---
+++ Tech Tips/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Tech_Tips.Startup))]
+namespace Tech_Tips
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
