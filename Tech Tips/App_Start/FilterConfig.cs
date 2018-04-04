677430b3816021c38b98feb85deff43b486aeec6
Muhammad Mohaiminul Islam <IamMohaiminul@Gmail.com>
2018-04-05 00:12:34 +0800

    Restricting Access​

--- Tech Tips/App_Start/FilterConfig.cs
+++ Tech Tips/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace Tech_Tips
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new AuthorizeAttribute());
         }
     }
 }
